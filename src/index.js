c133e69da03aa8d62822b4a396d3e422135e3fd4
Ciaran O'Connor <ciaranoconnor23@gmail.com>
2018-01-11 22:44:04 +0000

    persist the state to localstorage using redux persist

--- src/index.js
+++ src/index.js
@@ -1,17 +1,39 @@
 import 'babel-polyfill'; // generators
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { persistStore, persistReducer } from 'redux-persist'
+import { PersistGate } from 'redux-persist/lib/integration/react'
+import storage from 'redux-persist/lib/storage'
 import { render as renderReact } from 'react-dom';
 import { Provider } from 'react-redux'
 import { createStore } from 'redux'
-import reducer from './redux/reducers';
+import reducers from './redux/reducers';
 
 let App = require( './components/app' ).default;
 
-let store = createStore( reducer )
+const config = {
+  key: 'root',
+  storage,
+}
+
+const reducer = persistReducer( config, reducers )
+
+function configureStore() {
+  let store = createStore( reducer )
+  let persistor = persistStore( store )
+  return { persistor, store }
+}
+
+
+const { store, persistor } = configureStore()
 
 const render = ( Component ) => {
-  const comp = <Provider store={store}><Component /></Provider>
+  const comp =
+    <Provider store={store}>
+      <PersistGate persistor={persistor}>
+        <Component />
+      </PersistGate>
+    </Provider>
   renderReact( comp, document.getElementById( 'root' ) );
 };
 
@@ -19,10 +41,7 @@ render( App )
 
 if ( module.hot ) {
   module.hot.accept( './components/app', function () {
-    console.log( 'hh1' )
     let newApp = require( './components/app' ).default;
     render( newApp );
   } );
 }
-
-// ReactDOM.render( <App />, document.getElementById( 'root' ) );
