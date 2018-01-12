b913c97215f8dc896b9de3c8b2e04d8555e29b72
Ciaran O'Connor <ciaranoconnor23@gmail.com>
2018-01-12 19:30:53 +0000

    dont explode if no image found

--- src/components/CryptoPriceRow/CryptoPriceRow.js
+++ src/components/CryptoPriceRow/CryptoPriceRow.js
@@ -5,13 +5,22 @@ import styles from './CryptoPriceRow.css'
 
 export class CryptoPriceRow extends Component {
 
+  loadLogo( cryptoSymbol ) {
+    try {
+      return require( `../logos/${cryptoSymbol}.svg` )
+    } catch ( e ) {
+      return null
+    }
+  }
+
   render() {
     const { crypto, name, fiat, price, change } = this.props
     const numberFormatter = new Intl.NumberFormat();
     const changePositive = ( change > 0 ) ? 'text--green' : 'text--red'
+    const imgSrc = this.loadLogo( crypto )
     return (
       <tr>
-        <td><img src={require( `../logos/${crypto}.svg` )} alt="Crypto logo" width="20" height="20" /></td>
+        <td>{imgSrc && <img src={imgSrc} alt="Crypto logo" width="20" height="20" />}</td>
         <td>{`${name} (${crypto})`}</td>
         <td className="text--right"><span className={changePositive}>{fiatSymbol[fiat]}{numberFormatter.format( parseFloat( price ).toFixed( 4 ) ) || ""}</span></td>
         <td className="text--right"><span className={changePositive}>{change || ""}%</span></td>
