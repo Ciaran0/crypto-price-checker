265118d7b22e803afd2b4c734fbf78951010f3ce
Ciaran O'Connor <ciaranoconnor23@gmail.com>
2018-01-12 07:56:12 +0000

    format numbers

--- src/components/CryptoPriceRow/CryptoPriceRow.js
+++ src/components/CryptoPriceRow/CryptoPriceRow.js
@@ -7,13 +7,13 @@ export class CryptoPriceRow extends Component {
 
   render() {
     const { crypto, name, fiat, price, change } = this.props
-
+    const numberFormatter = new Intl.NumberFormat();
     const changePositive = ( change > 0 ) ? 'text--green' : 'text--red'
     return (
       <tr>
         <td><img src={require( `../logos/${crypto}.svg` )} alt="Crypto logo" width="20" height="20" /></td>
         <td>{`${name} (${crypto})`}</td>
-        <td className="text--right"><span className={changePositive}>{fiatSymbol[fiat]}{parseFloat( price ).toFixed( 4 ) || ""}</span></td>
+        <td className="text--right"><span className={changePositive}>{fiatSymbol[fiat]}{numberFormatter.format( parseFloat( price ).toFixed( 4 ) ) || ""}</span></td>
         <td className="text--right"><span className={changePositive}>{change || ""}%</span></td>
       </tr>
     );
